cd20c9ca05cd5ce2b5a101d873ad32974bf016cb
Ferdinand Engländer <ferdinand.englaender@concepts-and-training.de>
2022-08-22 11:56:09 +0200

    detailed result mockup basis

---
+++ src/UI/playground-TA-resultpage/included_data2-questiondata.php
@@ -0,0 +1,48 @@
+<?php
+/*
+function included_dataXY()
+{
+    return  array(
+        array(
+            'type' => 'Single Choice Frage',
+            'evaluation' => 'richtig beantwortet',
+            'question_title' => 'E.01.02.01.B Entwicklung: Grundfragen der Entwicklungspsychologie',
+            'question_txt' => '<h2>Welches sind zentrale Aufgaben der Entwicklungspsychologie?</h2><p><strong>Wählen Sie die zutreffenden Antworten aus.</strong> Mehrfachantworten sind möglich.</p>',
+            'answers' => array(
+                array('title' => 'Beschreibung und Erklärung von Entwicklungsveränderungen', 'status' => 'unchecked',	'evaluation' => 'richtig'),
+                array('title' => 'Bestimmung des aktuellen Entwicklungsstandes', 'status' => 'unchecked',	'evaluation' => 'richtig'),
+                array('title' => 'Prognosen über die zukünftige Entwicklung', 'status' => 'unchecked',	'evaluation' => 'richtig'),
+                array('title' => 'Beschreibung und Erklärung von intraindividuellen jedoch nicht interindividuellen Unterschieden', 'status' => 'unchecked',	'evaluation' => 'richtig'),
+                array('title' => 'Durchführung von Therapien', 'status' => 'unchecked',	'evaluation' => 'richtig')
+            ),
+            'stats' => array(
+                'total' => 10,
+                'skipped' => 2,
+                'most_common' => 2,
+                'most_common_total' => 6,
+                'median' => 2,
+            )
+        ),
+
+        array(
+            'type' => 'Single Choice Frage',
+            'evaluation' => 'richtig beantwortet',
+            'question_title' => 'E.03.05.01 Entwicklung: Kognitive und moralische Entwicklung',
+            'question_txt' => 'Sie führen mit einem Kind einen Versuch zur Mengenerhaltung mit Gefässen mit unterschiedlichem Umfang und unterschiedlicher Höhe durch. Ein Kind antwortet trotz konstant bleibender Flüssigkeitsmenge, dass dasjenige Gefäss mehr Flüssigkeit enthalte, bei dem der Pegel der Flüssigkeit höher steht. Nach Jean Piaget ist diese Aussage typisch für Kinder in der ...',
+            'answers' => array(
+                array('title' => '... formal-operationalen Entwicklungsphase', 'status' => 'unchecked',	'evaluation' => 'richtig'),
+                array('title' => '... konkret-operationalen Entwicklungsphase', 'status' => 'unchecked',	'evaluation' => 'richtig'),
+                array('title' => '... präoperationalen Entwicklungsphase', 'status' => 'unchecked',	'evaluation' => 'richtig'),
+                array('title' => '... sensumotorischen Entwicklungsphase', 'status' => 'unchecked',	'evaluation' => 'richtig')
+            ),
+            'stats' => array(
+                'total' => 10,
+                'skipped' => 2,
+                'most_common' => 2,
+                'most_common_total' => 6,
+                'median' => 2,
+            )
+        ),
+    );
+}
+*/
